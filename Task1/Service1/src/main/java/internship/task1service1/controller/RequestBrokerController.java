34f89c5aa2909761039a9db0c2d2b1ba55ab4d20
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2022-02-21 19:00:34 +0300

    Add HttpClient

--- Task1/Service1/src/main/java/internship/task1service1/controller/RequestBrokerController.java
+++ Task1/Service1/src/main/java/internship/task1service1/controller/RequestBrokerController.java
@@ -6,16 +6,32 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static internship.task1service1.controller.ResponseDataParser.getAnswer;
 
 @RestController
 public class RequestBrokerController {
     private final Logger logger =  LoggerFactory.getLogger(RequestBrokerController.class);
 
     @GetMapping
-    public CityModel[] makeRequest(@RequestParam String query){
-        logger.info("http://localhost:8090?query=" + query);
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject("http://localhost:8100?query=" + query.replace(' ', '+'), CityModel[].class);
+    public CityModel[] makeRequest(@RequestParam(defaultValue = "select * from city order by Population desc limit 5;") String query){
+        HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8100?query=" + query.replace(' ', '+'))).GET().build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            logger.info("Successful connect to http://localhost:8090?query=" + query);
+            return getAnswer(response.body());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            logger.info("Can't connect to http://localhost:8090?query=" + query);
+            return new CityModel[0];
+        }
     }
 }
