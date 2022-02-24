5fd63bc0b79011ab0c1aee8589a8783e77d41dcf
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2022-02-24 13:28:45 +0300

    Update code
    Add logging

--- Task1/Service1/src/main/java/internship/task1service1/controller/RequestBrokerController.java
+++ Task1/Service1/src/main/java/internship/task1service1/controller/RequestBrokerController.java
@@ -3,8 +3,7 @@ package internship.task1service1.controller;
 import internship.task1service1.httpClient.CityHttpClient;
 import internship.task1service1.model.CityModel;
 import internship.task1service1.service.RequestBrokerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,8 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class RequestBrokerController {
-    private final Logger logger =  LoggerFactory.getLogger(RequestBrokerController.class);
-
     private RequestBrokerService requestBrokerService;
 
     @Autowired
@@ -30,7 +27,6 @@ public class RequestBrokerController {
         if(!path.endsWith("/")){
             path = path.concat("/");
         }
-        System.out.println(path + id);
         return requestBrokerService.getResponseData(path + id);
     }
 }
--- Task1/Service1/src/main/java/internship/task1service1/service/RequestBrokerService.java
+++ Task1/Service1/src/main/java/internship/task1service1/service/RequestBrokerService.java
@@ -1,8 +1,11 @@
 package internship.task1service1.service;
 
+import internship.task1service1.controller.RequestBrokerController;
 import internship.task1service1.controller.ResponseDataParser;
 import internship.task1service1.httpClient.MyHttpClient;
 import internship.task1service1.model.CityModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.net.http.HttpResponse;
@@ -10,6 +13,7 @@ import java.util.Optional;
 
 @Service
 public class RequestBrokerService {
+    private final Logger logger =  LoggerFactory.getLogger(RequestBrokerController.class);
     private MyHttpClient myHttpClient;
 
     public RequestBrokerService(){}
@@ -21,9 +25,11 @@ public class RequestBrokerService {
     public CityModel[] getResponseData(String path){
         Optional<HttpResponse<String>> response = myHttpClient.makeRequest(path);
         if(response.isPresent()){
+            logger.info("Successful request to http://localhost:8100" + path);
             return ResponseDataParser.getAnswer(response.get().body());
         }
         else{
+            logger.info("Fail to make correct request to http://localhost:8100" + path);
             return new CityModel[0];
         }
     }
