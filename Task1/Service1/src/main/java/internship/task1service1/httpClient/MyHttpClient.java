c389e2f3eadff1820cc3b4c1510e25333f9a0f68
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2022-02-25 10:29:19 +0300

    Delete Task1/Service1/src/main/java/internship/task1service1/httpClient directory

--- Task1/Service1/src/main/java/internship/task1service1/httpClient/MyHttpClient.java
+++
@@ -1,9 +0,0 @@
-package internship.task1service1.httpClient;
-
-import java.net.http.HttpResponse;
-
-import java.util.Optional;
-
-public interface MyHttpClient {
-    public Optional<HttpResponse<String>> makeRequest(String path);
-}
