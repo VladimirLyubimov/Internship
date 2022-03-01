b716061b6b2609ec278ca277a35cd872909dad5e
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2022-03-01 09:53:46 +0300

    Remove excess files

--- Task1/task1service2/src/main/java/internship/task1service2/exceptions/DatabaseConnectionException.java
+++
@@ -1,7 +0,0 @@
-package internship.task1service2.exceptions;
-
-public class DatabaseConnectionException extends Exception{
-    public DatabaseConnectionException(String message){
-        super(message);
-    }
-}
